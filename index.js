40a3373e3e8c94314d0a53a7b8283d8cc4836c60
kushagra1201 <imt_2019053@iiitm.ac.in>
2023-03-12 13:11:17 +0530

    server template initialized

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hey there");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
